084816e46fd5f6cc942f2dce985271a6e3559009
Albert Wang <git@albertyw.com>
2018-05-13 01:02:49 -0700

    Output tech tree

--- parse.py
+++ parse.py
@@ -1,16 +1,33 @@
+import csv
 from slpp import slpp as lua
 
-with open('raw/technology.lua', 'r') as handle:
-    technology_list = handle.readlines()
 
-# Strip out unused logic
-x = 0
-while x < len(technology_list):
-    line = technology_list[0]
-    if line == 'data:extend(':
-        print(line)
-    x += 1
+with open('raw/technology2.lua', 'r') as handle:
+    technology_string = handle.read()
 
-technology_string = ''.join(technology_list)
 technology = lua.decode(technology_string)
-print(len(technology))
+
+
+def return_tech_dependency_tree(techs):
+    tree = {}
+    for x in range(len(techs)):
+        tech = techs[x]
+        if type(tech) == str:
+            continue
+        name = tech['name']
+        dependencies = tech.get('prerequisites', [])
+        tree[name] = dependencies
+    return tree
+
+
+def save_tech_dependency_tree(tree):
+    with open('data/technology.csv', 'w') as handle:
+        writer = csv.writer(handle)
+        for key, value in tree.items():
+            row = [key] + value
+            writer.writerow(row)
+
+
+tree = return_tech_dependency_tree(technology)
+save_tech_dependency_tree(tree)
+print(len(tree))
