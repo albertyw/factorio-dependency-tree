7d7d46e9972d439aec63c50000e648fa62f5a84b
Albert Wang <git@albertyw.com>
2019-03-02 23:50:47 -0800

    Ignore demo recipes

--- parse_recipe.py
+++ parse_recipe.py
@@ -7,6 +7,8 @@ from slpp import slpp as lua
 files = os.listdir('raw/recipe')
 recipes_string = ''
 for filename in files:
+    if filename[:5] == 'demo-':
+        continue
     with open('raw/recipe/%s' % filename, 'r') as handle:
         recipes_string += handle.read() + ','
 
